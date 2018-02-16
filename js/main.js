70e6756efe2f3c44846d6276ac1b69f5a9c556ea
Darek Spytkowski <sdarek19@gmail.com>
2018-02-16 14:30:49 +0100

    section about started

--- js/main.js
+++ js/main.js
@@ -19,3 +19,33 @@ $(document).on("scroll", function() {
 	}
 	
 });
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.hideme').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).addClass('showme');
+            }
+            if( bottom_of_window < bottom_of_object ){
+                $(this).removeClass('showme');
+            }
+        });
+    });
+});
+
+$(document).ready(function() {
+    $(window).scroll( function(){
+        $('.hidemore').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object ){
+                $(this).addClass('showmore');
+            }
+            if( bottom_of_window < bottom_of_object ){
+                $(this).removeClass('showmore');
+            }
+        });
+    });
+});
